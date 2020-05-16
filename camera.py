eb0583bd92c162b15c74f99db46f2a13d544bc8a
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-16 17:35:35 -0400

    Update camera.py

--- camera.py
+++ camera.py
@@ -3,20 +3,20 @@ import busio
 import board
 import adafruit_amg88xx
  
-  amg, err := amg8833.NewAMG8833(&amg8833.Opts{
-    Device: "/dev/i2c-1",
-    Mode:   amg8833.AMG88xxNormalMode,
-    Reset:  amg8833.AMG88xxInitialReset,
-    FPS:    amg8833.AMG88xxFPS10,
-  })
-  if err != nil {
-    panic(err)
-  }
+amg, err := amg8833.NewAMG8833(&amg8833.Opts{
+  Device: "/dev/i2c-1",
+  Mode:   amg8833.AMG88xxNormalMode,
+  Reset:  amg8833.AMG88xxInitialReset,
+  FPS:    amg8833.AMG88xxFPS10,
+})
+if err != nil {
+  panic(err)
+}
 
-  ticker := time.NewTicker(1 * time.Second)
+ticker := time.NewTicker(1 * time.Second)
 
-  for {
-    grid := amg.ReadPixels()
-    fmt.Println(grid)
-    <-ticker.C
-  }
+for {
+  grid := amg.ReadPixels()
+  fmt.Println(grid)
+  <-ticker.C
+}

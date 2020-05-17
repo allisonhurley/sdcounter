4bcf948eb74f0fc9cc79226d010cd09a2f5b003f
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-17 18:32:42 -0400

    Update camera_test.py

--- camera_test.py
+++ camera_test.py
@@ -17,18 +17,17 @@ count_flag = 0
 while True:     #if PIR sensor detects movement first, the person is entering. If cam detects person first, the person is leaving
    
     while temp_count != 0: #break out of loop when temp_count = 0
-        for row in amg.pixels: #check to see if camera detects person
-            for j in row:           #look through temps in a row
 
-            # count numbers in the list which are greater than 25
-                temp_count = sum(map(lambda x : x>25, amg.pixels))
 
-                if ((temp_count >= 2) and (count_flag == 0)):
-                    people_in_store = people_in_store + 1
-                    temp_count = 0 #reset temp count
-                    count_flag = 1 #set count flag so people not counted more than once
-                    break
-               break
+   # count numbers in the list which are greater than 25
+       temp_count = sum(map(lambda x : x>25, amg.pixels))
+
+       if ((temp_count >= 2) and (count_flag == 0)):
+           people_in_store = people_in_store + 1
+           temp_count = 0 #reset temp count
+           count_flag = 1 #set count flag so people not counted more than once
+                    
+              
         
     for row in amg.pixels:
         # Pad to 1 decimal place
@@ -37,7 +36,8 @@ while True:     #if PIR sensor detects movement first, the person is entering. I
     print("\n")
     
     
-    
+    count_flag = 0 #reset count flag
+    temp_count = 1 #reset condition for temp count
     print("There are ", people_in_store, " people in the store B.")
     print("")
     print("\n")

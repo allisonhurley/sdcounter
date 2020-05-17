8b9162d66e54df2197b71b1a873dead2de805b1a
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-17 13:42:11 -0400

    Update PIR_test.py

--- PIR_test.py
+++ PIR_test.py
@@ -9,14 +9,14 @@ while True:
 
     i=GPIO.input(11)
 
-        if i==0: #When output from motion sensor is LOW
+    if i==0: #When output from motion sensor is LOW
 
-            print "No intruders",i
-          
-            time.sleep(0.1)
+        print "No intruders",i
 
-        elif i==1: #When output from motion sensor is HIGH
+        time.sleep(0.1)
 
-            print "Intruder detected",i
-       
-            time.sleep(0.1)
+    elif i==1: #When output from motion sensor is HIGH
+
+        print "Intruder detected",i
+
+        time.sleep(0.1)

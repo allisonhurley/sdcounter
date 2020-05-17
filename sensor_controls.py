9a00e952bb38d4743aa53b7f09849f3463a8b66c
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-17 16:24:21 -0400

    Update sensor_controls.py

--- sensor_controls.py
+++ sensor_controls.py
@@ -11,38 +11,40 @@ pir = digitalio.DigitalInOut(board.D17)
 pir.direction = digitalio.Direction.INPUT
 pir.pull = digitalio.Pull.UP
 
-people_in_store = 0;
+people_in_store = 0
+temp_count = 0
 
 while True:     #if PIR sensor detects movement first, the person is entering. If cam detects person first, the person is leaving
 
-    if pir.value == False: #When output from motion sensor is LOW
-
-        for row in amg.pixels: #check to see if camera detects person
-            for j in row:           #look through temps in a row
-                if j > 22.0:        #detect a human temp (usually > 22 degrees celsius)
-
-                    time.sleep(.25) #time delay of quarter of a second
-                    if pir.value == True:
-                        if people_in_store > 0:
-                            people_in_store = people_in_store - 1 ## if it's not the last person in the store, subtract from count of people in store
-                    print("There are ", people_in_store, " people in the store A.")
-                break
-            #break
-
-    elif pir.value == True:               #When output from motion sensor is HIGH
-
-        time.sleep(.25)     #give 1/4s delay for person to enter building
-
-        for row in amg.pixels: #check to see if camera detects person
-            for j in row:           #look through temps in a row
-                if j > 22.0:        #detect a human temp (usually > 22 degrees celsius)
-                    people_in_store = people_in_store + 1 #add to count of people in store
+    for row in amg.pixels: #check to see if camera detects person
+        for j in row:           #look through temps in a row
+            if j > 22.0:        #detect a human temp (usually > 22 degrees celsius)
+                temp_count = temp_count + 1
+                time.sleep(.1) #time delay 
                 
-                print("There are ", people_in_store, " people in the store B.")
+                if pir.value == True && temp_count >= 2:
+                    if people_in_store > 0:
+                        people_in_store = people_in_store - 1 ## if it's not the last person in the store, subtract from count of people in store
+                print("There are ", people_in_store, " people in the store A.")
+            break
+        #break
+
+   if pir.value == True:               #When output from motion sensor is HIGH
+
+        time.sleep(.1)     #time delay
+
+        for row in amg.pixels: #check to see if camera detects person
+            for j in row:           #look through temps in a row
+                if j > 22.0:        #detect a human temp (usually > 22 degrees celsius)
+                    temp_count = temp_count + 1
+                    time.sleep(.1) #time delay 
+                    
+                    if temp_count >= 2:
+                        print("There are ", people_in_store, " people in the store B.")
                 break
         #break
 
-    print("There are ", people_in_store, " people in the store.")
+    print("There are ", people_in_store, " people in the store C.")
     print("")
     print("\n")
     time.sleep(.5) # 1/2 second delay 

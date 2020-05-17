ef4c7e2259c7a870def47ac74148caf426be44a0
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-16 20:36:12 -0400

    Update sensor_controls.py

--- sensor_controls.py
+++ sensor_controls.py
@@ -14,30 +14,30 @@ GPIO.setup(11, GPIO.IN)         #Read output from PIR motion sensor
 
 while True:     #if PIR sensor detects movement first, the person is entering. If cam detects person first, the person is leaving
     i=GPIO.input(11)
-        if i==0:                 #When output from motion sensor is LOW
-            print("No intruders",i)
-            for row in amg.pixels: #check to see if camera detects person
-                for j in row:           #look through temps in a row
-                    if j > 22.0:        #detect a human temp (usually > 22 degrees celsius)
-                       person_detected = 1
-                       time.sleep(.25)
-                        print("person leaving",j)
-                        break
-                break
-        
-        elif i==1:               #When output from motion sensor is HIGH
-            print("Intruder alert",i)
-            motion_detected = 1
-            time.sleep(.25)     #give 1/2 delay for person to enter building
-                
-            for row in amg.pixels: #check to see if camera detects person
-                for j in row:           #look through temps in a row
-                    if j > 22.0:        #detect a human temp (usually > 22 degrees celsius)
-                        person_detected = 1
-                        break
-                break
-            if person_detected == 1:
-                print("Person entering",j)
+    if i==0:                 #When output from motion sensor is LOW
+        print("No intruders",i)
+        for row in amg.pixels: #check to see if camera detects person
+            for j in row:           #look through temps in a row
+                if j > 22.0:        #detect a human temp (usually > 22 degrees celsius)
+                   person_detected = 1
+                   time.sleep(.25)
+                    print("person leaving",j)
+                    break
+            break
+
+    elif i==1:               #When output from motion sensor is HIGH
+        print("Intruder alert",i)
+        motion_detected = 1
+        time.sleep(.25)     #give 1/2 delay for person to enter building
+
+        for row in amg.pixels: #check to see if camera detects person
+            for j in row:           #look through temps in a row
+                if j > 22.0:        #detect a human temp (usually > 22 degrees celsius)
+                    person_detected = 1
+                    break
+            break
+        if person_detected == 1:
+            print("Person entering",j)
     
     print("")
     print("\n")

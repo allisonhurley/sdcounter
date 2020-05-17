d12ea58a7cc523bb8b7eb9e7ce77fb44750e845d
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-17 16:07:32 -0400

    Update sensor_controls.py

--- sensor_controls.py
+++ sensor_controls.py
@@ -11,19 +11,6 @@ pir = digitalio.DigitalInOut(board.D17)
 pir.direction = digitalio.Direction.INPUT
 pir.pull = digitalio.Pull.UP
 
-while True:
-  
-    if pir.value: #When output from motion sensor is HIGH
-
-        print ("Intruder detected",pir.value)
-
-        time.sleep(0.1)
-        
-    else: #When output from motion sensor is LOW
-
-        print ("No intruders",pir.value)
-
-        time.sleep(0.1)
 
 people_in_store = 0;
 

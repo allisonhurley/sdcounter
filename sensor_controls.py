85ebe957784916c54b0b95ea4fad6aed34128a8f
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-17 16:05:01 -0400

    Update sensor_controls.py

--- sensor_controls.py
+++ sensor_controls.py
@@ -1,36 +1,49 @@
 import time
 import busio
+import digitalio
 import board
 import adafruit_amg88xx
 
 i2c = busio.I2C(board.SCL, board.SDA)
 amg = adafruit_amg88xx.AMG88XX(i2c)
 
-import RPi.GPIO as GPIO
+pir = digitalio.DigitalInOut(board.D17)
+pir.direction = digitalio.Direction.INPUT
+pir.pull = digitalio.Pull.UP
 
-GPIO.setwarnings(False)
-GPIO.setup(11, GPIO.IN)         #Read output from PIR motion sensor
+ while True:
+  
+    if pir.value: #When output from motion sensor is HIGH
+
+        print ("Intruder detected",pir.value)
+
+        time.sleep(0.1)
+        
+    else: #When output from motion sensor is LOW
+
+        print ("No intruders",pir.value)
+
+        time.sleep(0.1)
 
 people_in_store = 0;
 
 while True:     #if PIR sensor detects movement first, the person is entering. If cam detects person first, the person is leaving
-    i=GPIO.input(11)
-    if i==0:                 #When output from motion sensor is LOW
-        
+    
+    if pir.value == False: #When output from motion sensor is LOW
+                       
         for row in amg.pixels: #check to see if camera detects person
             for j in row:           #look through temps in a row
                 if j > 22.0:        #detect a human temp (usually > 22 degrees celsius)
                                   
                    time.sleep(.25) #time delay of quarter of a second
-                   i=GPIO.input(11) #check to see if PIR sensor detects person leaving
-                   if i == 1:
+                   if pir.value == True:
                         if people_in_store > 0:
                             people_in_store -- ## if it's not the last person in the store, subtract from count of people in store
                
             break
         break
 
-    elif i==1:               #When output from motion sensor is HIGH
+    elif pir.value == True:               #When output from motion sensor is HIGH
 
         time.sleep(.25)     #give 1/4s delay for person to enter building
 
@@ -45,4 +58,4 @@ while True:     #if PIR sensor detects movement first, the person is entering. I
     print("There are ", people_in_store, " people in the store.")
     print("")
     print("\n")
-    time.sleep(1) # 1 second delay I think?
+    time.sleep(2) # 2 second delay 

4ba82aaf3661832762a7f061743abdfce2739b1e
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-17 18:28:42 -0400

    Update camera_test.py

--- camera_test.py
+++ camera_test.py
@@ -9,32 +9,34 @@ import adafruit_amg88xx
 i2c = busio.I2C(board.SCL, board.SDA)
 amg = adafruit_amg88xx.AMG88XX(i2c)
 
-temp_count = 0
+temp_count = 1
 people_in_store = 0
-row = 0
-col = 0
+count_flag = 0
+
 
 while True:     #if PIR sensor detects movement first, the person is entering. If cam detects person first, the person is leaving
-    
-    for row in amg.pixels: #check to see if camera detects person
-        for j in row:           #look through temps in a row
-            if j > 25.0:        #detect a human temp (usually > 22 degrees celsius)
-                temp_count = temp_count + 1
-                time.sleep(.1) #time delay
-                
-            if temp_count >= 2:
-                people_in_store = people_in_store + 1
-                temp_count = 0 #reset temp count
-                break
-            break
-    
+   
+    while(temp_count != 0) #break out of loop when temp_count = 0
+        for row in amg.pixels: #check to see if camera detects person
+            for j in row:           #look through temps in a row
+
+            # count numbers in the list which are greater than 25
+                temp_count = sum(map(lambda x : x>25, amg.pixels))
+
+                if ((temp_count >= 2) and (count_flag == 0)):
+                    people_in_store = people_in_store + 1
+                    temp_count = 0 #reset temp count
+                    count_flag = 1 #set count flag so people not counted more than once
+                    break
+               break
+        
     for row in amg.pixels:
         # Pad to 1 decimal place
         print(["{0:.1f}".format(temp) for temp in row])
         print("")
     print("\n")
     
-    print(amg.pixels[1][2])
+    
     
     print("There are ", people_in_store, " people in the store B.")
     print("")

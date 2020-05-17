470eed2a4d385f1c9f1372b21643bff15b929ff7
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-17 15:51:39 -0400

    Update and rename PIR_test.py to pir_test.py

---
+++ pir_test.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+pir_sensor = 17 #GPIO pin 17
+
+GPIO.setup(pir_sensor, GPIO.IN, GPIO.PUD_DOWN)
+
+current_state = 0
+
+while True:
+    try:
+        time.sleep(0.1)
+        current_state = GPIO.input(pir_sensor)
+        if current_state == 1:
+          print("GPIO pin %s is %s" % (pir_sensor, current_state)) # motion detected
+
+          time.sleep(4) # wait 4 seconds for PIR to reset. 
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        

87ccb46d9c1fedbb19d32a773bfb2a11f87352ea
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-18 14:50:37 -0400

    Update sensor_controls.py

--- sensor_controls.py
+++ sensor_controls.py
@@ -4,20 +4,33 @@ import digitalio
 import board
 import adafruit_amg88xx
 
-from datetime import datetime # for testing purposes
+
+import RPi.GPIO as GPIO
+
+
+GPIO.setmode(GPIO.BCM)
+PIR_PIN = 17
+GPIO.setup(PIR_PIN, GPIO.IN)
 
 i2c = busio.I2C(board.SCL, board.SDA)
 amg = adafruit_amg88xx.AMG88XX(i2c)
 
-pir = digitalio.DigitalInOut(board.D17)
-pir.direction = digitalio.Direction.INPUT
-pir.pull = digitalio.Pull.UP
-
-people_in_store = 0
+def MOTION(PIR_PIN):
+     motion_detected = 1
+        
+people_in_store = 0 #initialize flags to 0
 temp_count = 0
+motion_detected = 0
 
 while True:     #if PIR sensor detects movement first, the person is entering. If cam detects person first, the person is leaving
-    
+    try:
+        GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=MOTION)
+            while 1:
+                time.sleep(100)
+    except KeyboardInterrupt:
+        print("Quit")
+        GPIO.cleanup()
+
     new_list = sum(amg.pixels, [])
     count = sum(map(lambda x : x> 23, new_list))
 
@@ -29,16 +42,17 @@ while True:     #if PIR sensor detects movement first, the person is entering. I
             person_detected = 1 #detected person in front of camera flag set 
             count_flag = 1 #set count flag so people not counted more than once
                     
-    if (pir.value == True and person_detected == 1):
+    if (motion_detected == 1 and person_detected == 1):
         if people_in_store > 0:
             people_in_store = people_in_store - 1 ## if it's not the last person in the store, subtract from count of people in store
         
     count_flag = 0 #reset flags 
     person_detected = 0
+    motion_detected = 0
     
     time.sleep(0.3) #small time delay
        
-    if pir.value == True:               #When output from motion sensor is HIGH first NOT SURE IF IT SHOULD BE A WHILE OR IF???
+    if motion_detected==1:               #When output from motion sensor is HIGH first NOT SURE IF IT SHOULD BE A WHILE OR IF???
         new_list = sum(amg.pixels, [])
         count = sum(map(lambda x : x> 23, new_list))
         
@@ -53,6 +67,7 @@ while True:     #if PIR sensor detects movement first, the person is entering. I
                 
     count_flag = 0 #reset flags
     person_detected = 0 
+    motion_detected = 0
     print("There are ", people_in_store, " people in the store.")
     print("")
     print("\n")

7dea00522fdf973ae7046bed2e30d5f7252e8131
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-18 13:26:17 -0400

    Update pir_test.py

--- pir_test.py
+++ pir_test.py
@@ -12,4 +12,11 @@ print("PIR Module Test (CTRL+C to exit)")
 time.sleep(2)
 print("Ready")
 
+try:
+    GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=MOTION)
+    while 1:
+        time.sleep(100)
+except KeyboardInterrupt:
+    print("Quit")
+    GPIO.cleanup()
 

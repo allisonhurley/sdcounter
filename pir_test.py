179fe6f391af0910eb232ca252c3b330b016b954
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-18 12:43:21 -0400

    Update pir_test.py

--- pir_test.py
+++ pir_test.py
@@ -1,24 +1,26 @@
-import digitalio
-import time
-import board
+import RPi.GPIO as GPIO  
+GPIO.setmode(GPIO.BCM)  
+  
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
 
-
-pir = digitalio.DigitalInOut(board.D17)
-pir.direction = digitalio.Direction.INPUT
-pir.pull = digitalio.Pull.UP
-
- 
-while True:
-    prev_pir_val = pir.value
-    time.sleep(0.1) #small time delay of 200 ms
-    if pir.value == False and prev_pir_val == True: #When output from motion sensor is HIGH
-
-        print ("Intruder detected",pir.value)
-
-        time.sleep(0.1)
-        
-    else: #When output from motion sensor is LOW
-
-        print ("try again")
-
-        time.sleep(0.1)
+# now we'll define two threaded callback functions  
+# these will run in another thread when our events are detected  
+def my_callback(channel):  
+    print "falling edge detected on 17"  
+  
+def my_callback2(channel):  
+    print "falling edge detected on 23"  
+    
+# when a falling edge is detected on port 17, regardless of whatever   
+# else is happening in the program, the function my_callback will be run  
+GPIO.add_event_detect(17, GPIO.RISING, callback=my_callback, bouncetime=300)  
+  
+  
+try:  
+    print "Waiting for rising edge on port 17"  
+    GPIO.wait_for_edge(17, GPIO.RISING)  
+    print "Rising edge detected on port 17. Here endeth the third lesson."  
+  
+except KeyboardInterrupt:  
+    GPIO.cleanup()       # clean up GPIO on CTRL+C exit  
+GPIO.cleanup()           # clean up GPIO on normal exit  

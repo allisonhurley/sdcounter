64790358e30e2c453653c51a3a846f166f18f4e1
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-18 11:54:58 -0400

    Update sensor_controls.py

--- sensor_controls.py
+++ sensor_controls.py
@@ -16,38 +16,41 @@ temp_count = 0
 
 while True:     #if PIR sensor detects movement first, the person is entering. If cam detects person first, the person is leaving
     
-    for row in amg.pixels: #check to see if camera detects person
-        for j in row:           #look through temps in a row
-            if j > 22.0:        #detect a human temp (usually > 22 degrees celsius)
-                temp_count = temp_count + 1
-                time.sleep(.1) #time delay 
-                
-            if ((pir.value == True) and (temp_count >= 2)):
-                if people_in_store > 0:
-                    people_in_store = people_in_store - 1 ## if it's not the last person in the store, subtract from count of people in store
-            print("There are ", people_in_store, " people in the store A.")
-            temp_count = 0 #reset temp count
-            break
-        #break
+    new_list = sum(amg.pixels, [])
+    count = sum(map(lambda x : x> 23, new_list))
 
-    while pir.value == True:               #When output from motion sensor is HIGH
-
-        time.sleep(.1)     #time delay
-
-        for row in amg.pixels: #check to see if camera detects person
-            for j in row:           #look through temps in a row
-                if j > 22.0:        #detect a human temp (usually > 22 degrees celsius)
-                    temp_count = temp_count + 1
-                    time.sleep(.1) #time delay 
+    while count > 0: #while the camera detects a person's temperatures
+        new_list = sum(amg.pixels, []) #convert 2D array to list so we don't have to iterate
+        count = sum(map(lambda x: x>23, new_list)) #get number of temps in list greater than 23 degrees
+        if ((count >= 2) and (count_flag == 0)):
+            person_detected = 1 #detected person in front of camera flag set 
+            count_flag = 1 #set count flag so people not counted more than once
                     
-                if temp_count >= 2:
-                    people_in_store = people_in_store + 1
-                    print("There are ", people_in_store, " people in the store B.")
-                    temp_count = 0 #reset temp count
-                break
-        #break
+        if (pir.value == True and person_detected == 1):
+            if people_in_store > 0:
+                people_in_store = people_in_store - 1 ## if it's not the last person in the store, subtract from count of people in store
+        print("There are ", people_in_store, " people in the store A.")
+    
+    count_flag = 0 #reset flags 
+    person_detected = 0
+    
+    time.sleep(0.3) #small time delay
+       
+    while pir.value == True:               #When output from motion sensor is HIGH first NOT SURE IF IT SHOULD BE A WHILE OR IF???
+        new_list = sum(amg.pixels, [])
+        count = sum(map(lambda x : x> 23, new_list))
+        
+        while count > 0: #while the camera detects a person's temperatures
+            new_list = sum(amg.pixels, []) #convert 2D array to list so we don't have to iterate
+            count = sum(map(lambda x: x>23, new_list)) #get number of temps in list greater than 23 degrees
+            
+            if ((count >= 2) and (count_flag == 0)):
+                people_in_store = people_in_store + 1 
+                count_flag = 1 #set count flag so people not counted more than once
+       break # break out of pir loop             
+                
 
-    print("There are ", people_in_store, " people in the store C.")
+    print("There are ", people_in_store, " people in the store.")
     print("")
     print("\n")
     

d6cfec5d6156379de9457a4f267626cf3ad8829f
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-18 12:44:42 -0400

    Update pir_test.py

--- pir_test.py
+++ pir_test.py
@@ -6,10 +6,8 @@ GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 # now we'll define two threaded callback functions  
 # these will run in another thread when our events are detected  
 def my_callback(channel):  
-    print "falling edge detected on 17"  
+    print ("rising edge detected on 17")  
   
-def my_callback2(channel):  
-    print "falling edge detected on 23"  
     
 # when a falling edge is detected on port 17, regardless of whatever   
 # else is happening in the program, the function my_callback will be run  
@@ -17,9 +15,9 @@ GPIO.add_event_detect(17, GPIO.RISING, callback=my_callback, bouncetime=300)
   
   
 try:  
-    print "Waiting for rising edge on port 17"  
+    print ("Waiting for rising edge on port 17")  
     GPIO.wait_for_edge(17, GPIO.RISING)  
-    print "Rising edge detected on port 17. Here endeth the third lesson."  
+    print ("Rising edge detected on port 17. Here endeth the third lesson.")
   
 except KeyboardInterrupt:  
     GPIO.cleanup()       # clean up GPIO on CTRL+C exit  

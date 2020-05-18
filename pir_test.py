f70121445bae34af71b534b8ca8c382602558043
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-18 12:58:03 -0400

    Update pir_test.py

--- pir_test.py
+++ pir_test.py
@@ -1,20 +1,33 @@
-import time
-import digitalio
-import board
-import busio
+#!/usr/bin/env python2.7  
+# script by Alex Eames https://raspi.tv  
+# https://raspi.tv/how-to-use-interrupts-with-python-on-the-raspberry-pi-and-rpi-gpio-part-3  
+import RPi.GPIO as GPIO  
+GPIO.setmode(GPIO.BCM)  
+  
+# GPIO 23 & 17 set up as inputs, pulled up to avoid false detection.  
+# Both ports are wired to connect to GND on button press.  
+# So we'll be setting up falling edge detection for both  
 
-pir = digitalio.DigitalInOut(board.D17)
-pir.direction = digitalio.Direction.INPUT
-pir.pull = digitalio.Pull.UP
- 
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+  
 
-while True:
-    
-    if event.edge == pir.EDGE_FALLING:
-        print("intruder detected")
-        time.sleep(0.1)
-        
-    elif event.edge == pir.EDGE_RISING:
-        print("NONE")
-        time.sleep(0.1)
- 
+  
+# now we'll define two threaded callback functions  
+# these will run in another thread when our events are detected  
+def my_callback(channel):  
+    print "falling edge detected on 17"  
+  
+  
+# when a falling edge is detected on port 17, regardless of whatever   
+# else is happening in the program, the function my_callback will be run  
+GPIO.add_event_detect(17, GPIO.FALLING, callback=my_callback, bouncetime=300)  
+
+  
+try:  
+    print "Waiting for rising edge on port 17"  
+    GPIO.wait_for_edge(17, GPIO.FALLING)  
+    print "Rising edge detected on port 17. Here endeth the third lesson."  
+  
+except KeyboardInterrupt:  
+    GPIO.cleanup()       # clean up GPIO on CTRL+C exit  
+GPIO.cleanup()           # clean up GPIO on normal exit  

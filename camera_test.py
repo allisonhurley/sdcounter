ba7d0dca29f2f800c5516307f285250969f39a91
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-18 11:13:17 -0400

    Update camera_test.py

--- camera_test.py
+++ camera_test.py
@@ -19,12 +19,12 @@ while True:     #if PIR sensor detects movement first, the person is entering. I
    new_list = sum(amg.pixels, [])
    count = sum(map(lambda x : x> 23, new_list)
    
-   while count > 0:
-        new_list = sum(amg.pixels, [])
-        count = sum(map(lambda x: x>23, new_list)
-            if ((count >= 2) and (count_flag == 0)):
-                people_in_store = people_in_store + 1
-                count_flag = 1 #set count flag so people not counted more than once
+   #while count > 0:
+        #new_list = sum(amg.pixels, [])
+        #count = sum(map(lambda x: x>23, new_list)
+           # if ((count >= 2) and (count_flag == 0)):
+             #   people_in_store = people_in_store + 1
+               # count_flag = 1 #set count flag so people not counted more than once
      
    for row in amg.pixels:
       # Pad to 1 decimal place

56857593a25d703247e48c79ec107c1cc33635f1
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-18 11:24:23 -0400

    Update camera_test.py

--- camera_test.py
+++ camera_test.py
@@ -18,33 +18,22 @@ while True:     #if PIR sensor detects movement first, the person is entering. I
    
    new_list = sum(amg.pixels, [])
    count = sum(map(lambda x : x> 23, new_list))
-   if len(new_list.shape) == 1:
-      print('1-D array new_list')
-   if len(new_list.shape) == 2:
-      print('2-D array new_list')
-   if len(amg.pixels.shape) == 1:
-      print('1-D array')
-   if len(amg.pixels.shape) == 2:
-      print('2-D array')               
-   print("")
-   print("\n")               
-   time.sleep(1)
-   
-   #while count > 0:
-        #new_list = sum(amg.pixels, [])
-        #count = sum(map(lambda x: x>23, new_list)
-           # if ((count >= 2) and (count_flag == 0)):
-             #   people_in_store = people_in_store + 1
-               # count_flag = 1 #set count flag so people not counted more than once
+
+   while count > 0:
+        new_list = sum(amg.pixels, [])
+        count = sum(map(lambda x: x>23, new_list)
+            if ((count >= 2) and (count_flag == 0)):
+                people_in_store = people_in_store + 1
+                count_flag = 1 #set count flag so people not counted more than once
      
-   #for row in amg.pixels:
+   for row in amg.pixels:
       # Pad to 1 decimal place
-      #print(["{0:.1f}".format(temp) for temp in row])
-      #print("")
-   #print("\n")
+      print(["{0:.1f}".format(temp) for temp in row])
+      print("")
+   print("\n")
 
 
-   #count_flag = 0 #reset count flag
-   #print("There are ", people_in_store, " people in the store B.")
-   #print("")
-   #print("\n")
+   count_flag = 0 #reset count flag
+   print("There are ", people_in_store, " people in the store B.")
+   print("")
+   print("\n")

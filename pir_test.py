8afd02885c773b119d318fbce7721c05c987337e
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-18 12:36:53 -0400

    Update pir_test.py

--- pir_test.py
+++ pir_test.py
@@ -10,7 +10,7 @@ pir.pull = digitalio.Pull.UP
  
 while True:
     prev_pir_val = pir.value
-    time.sleep(0.2) #small time delay of 200 ms
+    time.sleep(0.4) #small time delay of 200 ms
     if pir.value == False and prev_pir_val == True: #When output from motion sensor is HIGH
 
         print ("Intruder detected",pir.value)

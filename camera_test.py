5d01577db747cc272553b9a6402ceafb88d225ee
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-18 11:26:37 -0400

    Update camera_test.py

--- camera_test.py
+++ camera_test.py
@@ -15,25 +15,25 @@ count_flag = 0
 
 
 while True:     #if PIR sensor detects movement first, the person is entering. If cam detects person first, the person is leaving
-   
-   new_list = sum(amg.pixels, [])
-   count = sum(map(lambda x : x> 23, new_list))
 
-   while count > 0:
+    new_list = sum(amg.pixels, [])
+    count = sum(map(lambda x : x> 23, new_list))
+
+    while count > 0:
         new_list = sum(amg.pixels, [])
         count = sum(map(lambda x: x>23, new_list))
-            if ((count >= 2) and (count_flag == 0)):
-                people_in_store = people_in_store + 1
-                count_flag = 1 #set count flag so people not counted more than once
-     
-   for row in amg.pixels:
-      # Pad to 1 decimal place
-      print(["{0:.1f}".format(temp) for temp in row])
-      print("")
-   print("\n")
+        if ((count >= 2) and (count_flag == 0)):
+            people_in_store = people_in_store + 1
+            count_flag = 1 #set count flag so people not counted more than once
+
+    for row in amg.pixels:
+    # Pad to 1 decimal place
+        print(["{0:.1f}".format(temp) for temp in row])
+        print("")
+    print("\n")
 
 
-   count_flag = 0 #reset count flag
-   print("There are ", people_in_store, " people in the store B.")
-   print("")
-   print("\n")
+    count_flag = 0 #reset count flag
+    print("There are ", people_in_store, " people in the store B.")
+    print("")
+    print("\n")

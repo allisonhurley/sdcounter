c520c5b5fcb81a5f1747fa2f322b0add8f1937bd
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-24 12:50:09 -0400

    Update pir_test.py

--- pir_test.py
+++ pir_test.py
@@ -3,9 +3,9 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 PIR_PIN = 17
-GPIO.setup(PIR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(PIR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.add_event_detect(PIR_PIN, GPIO.FALLING)
+GPIO.add_event_detect(PIR_PIN, GPIO.RISING)
 
 while True:
     if GPIO.event_detected(PIR_PIN): 

e9b46d0900ed61ea0172d6c73dfcd37e95566f3d
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-24 15:27:12 -0400

    Update sensor_controls.py

--- sensor_controls.py
+++ sensor_controls.py
@@ -34,15 +34,18 @@ while True:     #if PIR sensor detects movement first, the person is entering. I
 
         #wait for person to step out of camera detection area before comparing time stamps
         while count > 0: #while the camera detects a person's temperatures
-            new_list = sum(amg.pixels, []) #convert 2D array to list so we don't have to iterate
-            count = sum(map(lambda x: x>23, new_list)) #get number of temps in list greater than 23 degrees
-            print("You are stuck here")
-
-            if ((count > 0) and (count_flag == 0)): # if any number of temps > 23 FOR NOW COME BACK AND FIX?
+        
+            if ((count >= 2) and (count_flag == 0)): # if any number of temps > 23 FOR NOW COME BACK AND FIX?
                 camera_time = datetime.now()
                 print("camera time ", camera_time)
                 people_in_store += 1 #increment number of people in store
                 count_flag = 1 #set count flag so people not counted more than once
+        
+            new_list = sum(amg.pixels, []) #convert 2D array to list so we don't have to iterate
+            count = sum(map(lambda x: x>23, new_list)) #get number of temps in list greater than 23 degrees
+           
+
+
 
     count_flag = 0
     person_detected = 0
@@ -52,18 +55,18 @@ while True:     #if PIR sensor detects movement first, the person is entering. I
     count = sum(map(lambda x : x> 23, new_list))
 
     while count > 0: #while the camera detects a person's temperatures
-
+       
         if ((count >= 2) and (count_flag == 0)):
             person_detected = 1 #detected person in front of camera flag set 
             count_flag = 1 #set count flag so people not counted more than once
 
-    if (GPIO.event_detected(PIR_PIN) and person_detected==1):                
-        if people_in_store > 0:
-            people_in_store -= 1 ## if it's not the last person in the store, subtract from count of people in store
-
         new_list = sum(amg.pixels, []) #convert 2D array to list so we don't have to iterate
         count = sum(map(lambda x: x>23, new_list)) #get number of temps in list greater than 23 degrees
 
+    if (GPIO.event_detected(PIR_PIN) and person_detected==1):                
+    if people_in_store > 0:
+        people_in_store -= 1 ## if it's not the last person in the store, subtract from count of people in store
+   
     count_flag = 0 #reset flags 
     person_detected = 0
 

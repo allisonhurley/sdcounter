4e0e30c2bf6e0f4ee6a82a2b78bea664f2a85f48
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-24 15:41:19 -0400

    Update pir_test.py

--- pir_test.py
+++ pir_test.py
@@ -6,8 +6,6 @@ GPIO.setmode(GPIO.BCM)
 PIR_PIN = 17
 GPIO.setup(PIR_PIN, GPIO.IN)
 
-GPIO.add_event_detect(PIR_PIN, GPIO.BOTH, callback=MOTION)
-motion_detected = 0
 
 def MOTION(PIR_PIN):
     if GPIO.input(17):     # if port 17 == 1  
@@ -17,8 +15,9 @@ def MOTION(PIR_PIN):
         PIR_falling = datetime.now()
         print("Falling edge detected on 17 ",PIR_falling) 
 
-
+GPIO.add_event_detect(PIR_PIN, GPIO.BOTH, callback=MOTION)
 motion_detected = 0
+
 
 while True:
     

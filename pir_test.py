483ee8e3cd00fc1a0e34e221a3909955e1dea999
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-24 12:43:44 -0400

    Update pir_test.py

--- pir_test.py
+++ pir_test.py
@@ -3,20 +3,11 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 PIR_PIN = 17
-GPIO.setup(PIR_PIN, GPIO.IN)
+GPIO.setup(PIR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def MOTION(PIR_PIN):
-     print("Motion Detected")
+GPIO.add_event_detect(PIR_PIN, GPIO.FALLING)
 
-print("PIR Module Test (CTRL+C to exit)")
-time.sleep(2)
-print("Ready")
-
-try:
-    GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=MOTION)
-    while 1:
-        time.sleep(100)
-except KeyboardInterrupt:
-    print("Quit")
-    GPIO.cleanup()
+while True:
+    if GPIO.event_detected(PIR_PIN): 
+        print("Motion detected")    
 

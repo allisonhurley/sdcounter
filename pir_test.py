3219cc1280ef67e3101a95aa3cc8205e75edcc3f
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-24 19:10:03 -0400

    Update pir_test.py

--- pir_test.py
+++ pir_test.py
@@ -3,22 +3,43 @@ import time
 from datetime import datetime
 
 GPIO.setmode(GPIO.BCM)
-PIR_PIN = 17
-GPIO.setup(PIR_PIN, GPIO.IN)
+pir1 = 17
+pir2 = 27
+GPIO.setup(pir1, GPIO.IN)
+GPIO.setup(pir2, GPIO.IN)
 
+def PIR1MOTION(pir1):  
+    pir1_time = datetime.now()
+    pir1_flag = 1
+    
+def PIR2MOTION(pir2):
+    pir2_time = datetime.now()
+    pir2_flag = 1
 
-def MOTION(PIR_PIN):
-    if GPIO.input(17):     # if port 17 == 1  
-        PIR_rising = datetime.now()
-        print("Rising edge detected on 17 ",PIR_rising)  
-    else:                  # if port 17 != 1  
-        PIR_falling = datetime.now()
-        print("Falling edge detected on 17 ",PIR_falling) 
+GPIO.add_event_detect(pir1, GPIO.RISING, callback=PIR1MOTION)
+GPIO.add_event_detect(pir2, GPIO.RISING, callback=PIR2MOTION)
 
-GPIO.add_event_detect(PIR_PIN, GPIO.BOTH, callback=MOTION)
-motion_detected = 0
-
+#initialize time values to be equal
+pir1_time = datetime.now()
+pir2_time = pir1_time
+people_in_store = 10
+pir1_flag = 0
+pir2_flag = 0
 
 while True:
+    if (pir1_flag == 1) and (pir2_flag == 1):
+        if pir1_time > pir2_time:
+            if people_in_store > 0:
+                people_in_store -= 1
+        elif pir1_time < pir2_time:
+            people_in_store += 1
+        else:
+            people_in_store = people_in_store
+
+        pir1_time = pir_2_time #reset flags to be equal until next rising edge
+        pir1_flag = 0
+        pir2_flag = 0
     
-    time.sleep(.1)
+    print("There are ", people_in_store, "in the store")
+    print("")
+    print("\n")

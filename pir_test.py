9ea65d9a3d6761209c4249d46cf1a64b7cbb1246
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-24 13:05:33 -0400

    Update pir_test.py

--- pir_test.py
+++ pir_test.py
@@ -1,14 +1,23 @@
 import RPi.GPIO as GPIO
 import time
+from datetime import datetime
 
 GPIO.setmode(GPIO.BCM)
 PIR_PIN = 17
-GPIO.setup(PIR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(PIR_PIN, GPIO.IN)
 
-GPIO.add_event_detect(PIR_PIN, GPIO.RISING)
-
-while True:
-    if GPIO.event_detected(PIR_PIN): 
-        print("Motion detected")    
+def MOTION(PIR_PIN):
+    if GPIO.input(17):     # if port 17 == 1  
+        rising_time = datetime.now()
+        print("Rising edge detected on 17 ",rising_time)  
+    else:                  # if port 17 != 1  
+        falling_time = datetime.now()
+        print("Falling edge detected on 17 ", falling_time) 
+try:
+    GPIO.add_event_detect(PIR_PIN, GPIO.BOTH, callback=MOTION)
+    while 1:
+        time.sleep(100)
         
-    time.sleep(1)
+except KeyboardInterrupt:
+    print("Quit")
+    GPIO.cleanup()

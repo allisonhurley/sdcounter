a0006a60e2f6e1de57fc2228c2c748c1a658ccae
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-24 12:24:37 -0400

    Update sensor_controls.py

--- sensor_controls.py
+++ sensor_controls.py
@@ -30,36 +30,38 @@ while True:     #if PIR sensor detects movement first, the person is entering. I
     while count > 0: #while the camera detects a person's temperatures
         new_list = sum(amg.pixels, []) #convert 2D array to list so we don't have to iterate
         count = sum(map(lambda x: x>23, new_list)) #get number of temps in list greater than 23 degrees
+        
         if ((count >= 2) and (count_flag == 0)):
             person_detected = 1 #detected person in front of camera flag set 
             count_flag = 1 #set count flag so people not counted more than once
-    if (GPIO.event_detected(channel) and person_detected==1):                
-        if people_in_store > 0:
-        people_in_store = people_in_store - 1 ## if it's not the last person in the store, subtract from count of people in store
+        
+        if (GPIO.event_detected(channel) and person_detected==1):                
+            if people_in_store > 0:
+                people_in_store = people_in_store - 1 ## if it's not the last person in the store, subtract from count of people in store
 
     count_flag = 0 #reset flags 
     person_detected = 0
-    
-    
+
+
     time.sleep(0.3) #small time delay
-       
+
     if GPIO.event_detected(channel):          #When output from motion sensor is HIGH first NOT SURE IF IT SHOULD BE A WHILE OR IF???
         new_list = sum(amg.pixels, [])
         count = sum(map(lambda x : x> 23, new_list))
-        
+
         while count > 0: #while the camera detects a person's temperatures
             new_list = sum(amg.pixels, []) #convert 2D array to list so we don't have to iterate
             count = sum(map(lambda x: x>23, new_list)) #get number of temps in list greater than 23 degrees
-            
-            if ((count >= 2) and (count_flag == 0)):
-                people_in_store = people_in_store + 1 
-                count_flag = 1 #set count flag so people not counted more than onc
-                       
-                
+
+        if ((count >= 2) and (count_flag == 0)):
+            people_in_store = people_in_store + 1 
+            count_flag = 1 #set count flag so people not counted more than onc
+
+
     count_flag = 0 #reset flags
     person_detected = 0 
 
     print("There are ", people_in_store, " people in the store.")
     print("")
     print("\n")
-    
+    time.sleep(0.3) #small time delay    

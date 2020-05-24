942e7b3fee3117c7f66d2c99edee7756503aed6c
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-24 19:30:28 -0400

    Update pir_test.py

--- pir_test.py
+++ pir_test.py
@@ -8,14 +8,14 @@ PIR_PIN2 = 27
 GPIO.setup(PIR_PIN1, GPIO.IN)
 GPIO.setup(PIR_PIN2, GPIO.IN)
 
-def MOTION(PIR_PIN1):
+def MOTION1(PIR_PIN1):
     if GPIO.input(17):     # if port 17 == 1  
         rising_time = datetime.now()
         print("Rising edge detected on 17 ",rising_time)  
     else:                  # if port 17 != 1  
         falling_time = datetime.now()
         print("Falling edge detected on 17 ", falling_time) 
-def MOTION(PIR_PIN1):
+def MOTION2(PIR_PIN2):
     if GPIO.input(17):     # if port 17 == 1  
         rising_time = datetime.now()
         print("Rising edge detected on 17 ",rising_time)  
@@ -23,7 +23,8 @@ def MOTION(PIR_PIN1):
         falling_time = datetime.now()
         print("Falling edge detected on 17 ", falling_time) 
 try:
-    GPIO.add_event_detect(PIR_PIN, GPIO.BOTH, callback=MOTION)
+    GPIO.add_event_detect(PIR_PIN1, GPIO.BOTH, callback=MOTION1)
+    GPIO.add_event_detect(PIR_PIN2, GPIO.BOTH, callback=MOTION2)
     while 1:
         time.sleep(100)
         

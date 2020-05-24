66d6c4da743eed7620a544c4f2578a8464d2d9cf
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-24 12:21:20 -0400

    Update sensor_controls.py

--- sensor_controls.py
+++ sensor_controls.py
@@ -14,20 +14,17 @@ GPIO.setup(PIR_PIN, GPIO.IN)
 
 i2c = busio.I2C(board.SCL, board.SDA)
 amg = adafruit_amg88xx.AMG88XX(i2c)
-
-def MOTION(PIR_PIN):
-     motion_detected = 1
         
 people_in_store = 0 #initialize flags to 0
-temp_count = 0
-motion_detected = 0
+count = 0
+count_flag = 0 
 
-GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=MOTION)
+GPIO.add_event_detect(PIR_PIN, GPIO.RISING) #we want to detect rising edge of PIR sensor
 
 while True:     #if PIR sensor detects movement first, the person is entering. If cam detects person first, the person is leaving
 
 
-    new_list = sum(amg.pixels, [])
+    new_list = sum(amg.pixels, [])   #converts 2D array to list and counts number of temps > 23 in list
     count = sum(map(lambda x : x> 23, new_list))
 
     while count > 0: #while the camera detects a person's temperatures
@@ -36,18 +33,17 @@ while True:     #if PIR sensor detects movement first, the person is entering. I
         if ((count >= 2) and (count_flag == 0)):
             person_detected = 1 #detected person in front of camera flag set 
             count_flag = 1 #set count flag so people not counted more than once
-                    
-    if (motion_detected == 1 and person_detected == 1):
+    if (GPIO.event_detected(channel) and person_detected==1):                
         if people_in_store > 0:
-            people_in_store = people_in_store - 1 ## if it's not the last person in the store, subtract from count of people in store
-        
+        people_in_store = people_in_store - 1 ## if it's not the last person in the store, subtract from count of people in store
+
     count_flag = 0 #reset flags 
     person_detected = 0
-    motion_detected = 0
+    
     
     time.sleep(0.3) #small time delay
        
-    if motion_detected==1:               #When output from motion sensor is HIGH first NOT SURE IF IT SHOULD BE A WHILE OR IF???
+    if GPIO.event_detected(channel):          #When output from motion sensor is HIGH first NOT SURE IF IT SHOULD BE A WHILE OR IF???
         new_list = sum(amg.pixels, [])
         count = sum(map(lambda x : x> 23, new_list))
         
@@ -62,7 +58,7 @@ while True:     #if PIR sensor detects movement first, the person is entering. I
                 
     count_flag = 0 #reset flags
     person_detected = 0 
-    motion_detected = 0
+
     print("There are ", people_in_store, " people in the store.")
     print("")
     print("\n")

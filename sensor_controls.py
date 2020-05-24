6e3c45930e88792e4eb7404e98ea16a3cc726edc
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-24 14:38:52 -0400

    Update sensor_controls.py

--- sensor_controls.py
+++ sensor_controls.py
@@ -31,24 +31,24 @@ while True:     #if PIR sensor detects movement first, the person is entering. I
             pir_rising = datetime.now()
             print("PIR rising time ",pir_rising)
         
-        new_list = sum(amg.pixels, [])   #converts 2D array to list and counts number of temps > 23 in list
-        count = sum(map(lambda x : x> 23, new_list))
+            new_list = sum(amg.pixels, [])   #converts 2D array to list and counts number of temps > 23 in list
+            count = sum(map(lambda x : x> 23, new_list))
 
-        if ((count > 0) and (count_flag == 0)): # if any number of temps > 23 FOR NOW COME BACK AND FIX?
-            camera_time = datetime.now()
-            print("camera time ", camera_time)
-            count_flag = 1 #set count flag so people not counted more than once
-            
-        #wait for person to step out of camera detection area before comparing time stamps
-            while count > 0: #while the camera detects a person's temperatures
-                new_list = sum(amg.pixels, []) #convert 2D array to list so we don't have to iterate
-                count = sum(map(lambda x: x>23, new_list)) #get number of temps in list greater than 23 degrees
+            if ((count > 0) and (count_flag == 0)): # if any number of temps > 23 FOR NOW COME BACK AND FIX?
+                camera_time = datetime.now()
+                print("camera time ", camera_time)
+                count_flag = 1 #set count flag so people not counted more than once
+
+            #wait for person to step out of camera detection area before comparing time stamps
+                while count > 0: #while the camera detects a person's temperatures
+                    new_list = sum(amg.pixels, []) #convert 2D array to list so we don't have to iterate
+                    count = sum(map(lambda x: x>23, new_list)) #get number of temps in list greater than 23 degrees
 
 
-            if camera_time > pir_rising:
-                people_in_store += 1
-            elif (camera_time < pir_rising) and (people_in_store > 0):
-                people_in_store -= 1
+                if camera_time > pir_rising:
+                    people_in_store += 1
+                elif (camera_time < pir_rising) and (people_in_store > 0):
+                    people_in_store -= 1
             
 
     

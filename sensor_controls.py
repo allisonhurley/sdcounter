232d60c4b9550c64187d90f0de90241d9f1290ad
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-24 20:27:10 -0400

    Update sensor_controls.py

--- sensor_controls.py
+++ sensor_controls.py
@@ -7,9 +7,23 @@ import board
 import adafruit_amg88xx
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
-PIR_PIN = 17
-GPIO.setup(PIR_PIN, GPIO.IN)
+pir1 = 17
+pir2 = 27
+GPIO.setup(pir1, GPIO.IN)
+GPIO.setup(pir2, GPIO.IN)
+
+def PIR1MOTION(pir1):  
+    pir1_time = datetime.now()
+    pir1_flag = 1
+    print("pir1 time ", pir1_time)
+
+def PIR2MOTION(pir2):
+    pir2_time = datetime.now()
+    pir2_flag = 1
+    print("pir2 time ", pir2_time)
+    
+GPIO.add_event_detect(pir1, GPIO.RISING, callback=PIR1MOTION)
+GPIO.add_event_detect(pir2, GPIO.RISING, callback=PIR2MOTION)
 
 i2c = busio.I2C(board.SCL, board.SDA)
 amg = adafruit_amg88xx.AMG88XX(i2c)
@@ -17,23 +31,11 @@ amg = adafruit_amg88xx.AMG88XX(i2c)
 people_in_store = 0 #initialize flags to 0
 count = 0
 count_flag = 0 
-test_flag1 = 0
-test_flag2 = 0
-        
-GPIO.add_event_detect(PIR_PIN, GPIO.BOTH)
 
-while True:     #if PIR sensor detects movement first, the person is entering. If cam detects person first, the person is leaving
+pir1_flag = 0
+pir2_flag = 0        
 
-  
-    #CASE 1: PIR SENSOR FIRST THEN CAMERA DETECTION
-    if GPIO.event_detected(PIR_PIN):
-        if GPIO.input(17):     # if port 17 == 1  
-            pir_rising = datetime.now()
-            test_flag1 = 1
-            print("Rising edge detected on 17 ",pir_rising)  
-        else:                  # if port 17 != 1  
-            pir_falling = datetime.now()
-            print("Falling edge detected on 17 ",pir_falling) 
+while True:     #if PIR sensor detects movement first, the person is entering. If cam detects person first, the person is leaving 
       
     new_list = sum(amg.pixels, [])   #converts 2D array to list and counts number of temps > 23 in list
     count = sum(map(lambda x : x> 23, new_list))
@@ -43,24 +45,23 @@ while True:     #if PIR sensor detects movement first, the person is entering. I
 
         if ((count > 0) and (count_flag == 0)): # if any number of temps > 23 FOR NOW COME BACK AND FIX?
             camera_time = datetime.now()
-            test_flag2 = 1
             print("camera time ", camera_time)
             count_flag = 1 #set count flag so people not counted more than once
-
+        
+        if (pir1_flag == 1) and (count_flag = 1):
+            if (people_in_store > 0):
+                people_in_store -= 1
+        elif (pir2_flag == 1) and (count_flag = 1):
+            people_in_store += 1
+        
         new_list = sum(amg.pixels, []) #convert 2D array to list so we don't have to iterate
         count = sum(map(lambda x: x>23, new_list)) #get number of temps in list greater than 23 degrees
-
-    if ((test_flag1 == 1) and (test_flag2 == 1)):
-        if camera_time > PIR_rising:
-            people_in_store += 1
-        elif (camera_time < PIR_rising) and (people_in_store > 0):
-            people_in_store -= 1
+        
         
     #reset flags
     count_flag = 0
-    person_detected = 0
-    test_flag1 = 0
-    test_flag2 = 0
+    pir1_flag = 0
+    pir2_flag = 0 
         
     
     print("There are ", people_in_store, " people in the store.")

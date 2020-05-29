947ffa0683e4fa5072a03a3f4758f7d9a347ce2f
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-29 11:08:54 -0400

    Update sensor_controls.py

--- sensor_controls.py
+++ sensor_controls.py
@@ -8,11 +8,9 @@ import adafruit_amg88xx
 import RPi.GPIO as GPIO
 
 #for sending count to server
-import sys
 import requests
 
-id = sys.argv[1] if len(sys.argv) > 1 else 1
-count = sys.argv[2] if len(sys.argv) > 2 else 2
+id = "Door 1"
 
 #set up gpio 
 pir1 = 17
@@ -39,17 +37,18 @@ while True:     #if PIR sensor detects movement first, the person is entering. I
     new_list = sum(amg.pixels, [])   #converts 2D array to list and counts number of temps > 23 in list
     count = sum(map(lambda x : x> 23, new_list))
 
-        #wait for person to step out of camera detection area before comparing time stamps
-    while count > 0: #while the camera detects a person's temperatures
+        #wait for person to step out of camera detection area before checking pir flags
+    while count > 0: 
 
         if ((count > 0) and (count_flag == 0)): # if any number of temps > 23 FOR NOW COME BACK AND FIX?
             camera_time = datetime.now()
             print("camera time ", camera_time)
             count_flag = 1 #set count flag so people not counted more than once
         
-        new_list = sum(amg.pixels, []) #convert 2D array to list so we don't have to iterate
+        new_list = sum(amg.pixels, []) #convert 2D array to list so we don't have to iterate through array
         count = sum(map(lambda x: x>23, new_list)) #get number of temps in list greater than 23 degrees
    
+    #wait for rising edge of one of the pir sensors
     while((pir1_flag == 0 and pir2_flag == 0) and count_flag == 1):
         if GPIO.event_detected(pir1):
             pir1_time = datetime.now()
@@ -68,10 +67,17 @@ while True:     #if PIR sensor detects movement first, the person is entering. I
            print("pir1 time ", pir1_time) 
        if pir2_flag == 1:
            print("pir2 time ", pir2_time) 
+       
+       resp = requests.put(f'http://allisonhurley.com/api/rooms/{id}/count/{people_in_store}')
+       if resp.status_code != 204:
+       # This means something went wrong.
+       raise RuntimeError(f'PUT /api/rooms/{id}/count/{people_in_store} {resp.status_code}')
+    
        print("There are ", people_in_store, " people in the store.")
        print("")
        print("\n")
        prev_people = people_in_store
+    
     #reset flags
     count_flag = 0
     pir1_flag = 0

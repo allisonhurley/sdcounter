226d72561a5f6f5664c27d1dfc1e910201c18329
Allison Hurley <allison@hurleyhome.com>
2020-05-29 16:31:41 +0000

    Add query support

--- streamlit.py
+++ streamlit.py
@@ -1,11 +1,14 @@
 import time
+import datetime
 import numpy as np
 import pandas as pd
 import streamlit as st
 
 @st.cache
-def get_room_data(room_id):
-    url = f'http://allisonhurley.com/api/journal/{room_id}'
+def get_room_data(room_id,before,after):
+    url = f'http://allisonhurley.com/api/journal/?before={before}&after={after}'
+    if len(room_id) > 0:
+       url = f'http://allisonhurley.com/api/journal/?rooms={room_id}&before={before}&after={after}'
     return pd.read_json(url)
 
 @st.cache
@@ -20,11 +23,20 @@ This is some _markdown_.
 Go to [homepage](http://allisonhurley.com)
 '''
 
-rooms = get_rooms()
-st.write(rooms)
+room_data = get_rooms()
+def get_label(row):
+    return room_data[room_data['id'] == row].iloc[0,1]
 
-df = get_room_data(1)
-st.write('Test me', df)
+start = st.date_input('Start Date', datetime.date(2020,1,1))
+stop = st.date_input('Stop Date', datetime.datetime.now())
+
+rooms = st.multiselect(
+        "Select rooms", room_data['id'], format_func=get_label
+)
+rooms = '-'.join(map(str,rooms))
+rooms = get_room_data(rooms,stop,start)
+
+st.write(rooms)
 
 #progress_bar = st.sidebar.progress(0)
 #status_text = st.sidebar.empty()

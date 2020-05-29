64ce9ad6e6e9013d8ceb65827bb95bddf59db380
ericatownsend <58475797+ericatownsend@users.noreply.github.com>
2020-05-29 17:19:26 -0400

    Update camera_test.py

--- camera_test.py
+++ camera_test.py
@@ -2,7 +2,6 @@
 
 import time
 import busio
-import numpy as np
 import board
 import adafruit_amg88xx
 
@@ -11,10 +10,11 @@ amg = adafruit_amg88xx.AMG88XX(i2c)
 
 count = 0
 people_in_store = 0
+prev_people = 0
 count_flag = 0
 
 
-while True:     #if PIR sensor detects movement first, the person is entering. If cam detects person first, the person is leaving
+while True:   
 
     new_list = sum(amg.pixels, [])
     count = sum(map(lambda x : x> 23, new_list))
@@ -25,15 +25,17 @@ while True:     #if PIR sensor detects movement first, the person is entering. I
         if ((count >= 2) and (count_flag == 0)):
             people_in_store = people_in_store + 1
             count_flag = 1 #set count flag so people not counted more than once
+            prev_people = people_in_store
 
-    for row in amg.pixels:
-    # Pad to 1 decimal place
-        print(["{0:.1f}".format(temp) for temp in row])
+    if people_in_store != prev_people:
+        for row in amg.pixels:
+        # Pad to 1 decimal place
+            print(["{0:.1f}".format(temp) for temp in row])
+            print("")
+        print("\n")
+
+        print("There are {people_in_store} people in the store.")
         print("")
-    print("\n")
-
-
-    count_flag = 0 #reset count flag
-    print("There are ", people_in_store, " people in the store B.")
-    print("")
-    print("\n")
+        print("\n")
+    
+    count_flag = 0 #reset flag
